07808b11217b0af2215558ae1a47e8ae66c59fe7
Mendu Sai krishna <mskrishna2808@gmail.com>
2022-10-26 17:10:26 +0530

    Added code for zoning steps

--- zoning-creation/zoning-creation.sh
+++ zoning-creation/zoning-creation.sh
@@ -8,5 +8,14 @@ cat zoning-creation/input.txt | while read column1 column2 column3 ; do
     echo "zonecreate "\"$column1\_$column3\", \"$column1\;$column3\""" >> zoning-creation/output.txt
 done
 
+echo "============================================================" >> zoning-creation/output.txt
 
-#          echo "The End" | tee -a input.txt
+cat zoning-creation/input.txt | while read column1 column2 column3 ; do
+    echo "cfgadd "\"$column4\", \"$column1\_$column3\""" >> zoning-creation/output.txt
+done
+
+echo "============================================================" >> zoning-creation/output.txt
+
+cfg save >> zoning-creation/output.txt
+
+cfg enable >> zoning-creation/output.txt
