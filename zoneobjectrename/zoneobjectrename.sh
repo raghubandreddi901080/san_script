4ff527849edd16f97816603b73fd5c610ad84efe
Mendu Sai krishna <mskrishna2808@gmail.com>
2022-10-26 16:10:39 +0530

    Created folder for zoneobject rename

--- zoneobjectrename/zoneobjectrename.sh
+++ zoneobjectrename/zoneobjectrename.sh
@@ -1,5 +1,5 @@
 cat input.txt | while read column1 column2 ; do
-    echo "zoneobjectrename "\"$column1\", \"$column2\""" >> zoneobjectrename/output.txt
+    echo "zoneobjectrename "\"$column1\", \"$column2\""" >> output.txt
 done
 
 
