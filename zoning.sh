4b0cf1c35bae7299c8ebcbe5315007c8be29811a
Mendu Sai krishna <mskrishna2808@gmail.com>
2022-10-26 13:56:54 +0530

    modified script

--- zoning.sh
+++ zoning.sh
@@ -1,3 +1,3 @@
-cat zoning_info.txt | while read column1 column2 ; do
-    echo "alicreate "\"$column1\", \"$column2\""" >> /home/runner/work/san_script/san_script/output.txt
+cat input.txt | while read column1 column2 ; do
+    echo "alicreate "\"$column1\", \"$column2\""" >> output.txt
 done
