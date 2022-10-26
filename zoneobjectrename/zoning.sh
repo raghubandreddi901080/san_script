ee9bec6d1a79c54c9aaadc437b804ad1933be18a
Mendu Sai krishna <mskrishna2808@gmail.com>
2022-10-26 15:48:33 +0530

    Created folder for zoneobject rename

--- zoneobjectrename/zoning.sh
+++ zoneobjectrename/zoning.sh
@@ -1,5 +1,5 @@
 cat input.txt | while read column1 column2 ; do
-    echo "alicreate "\"$column1\", \"$column2\""" >> output.txt
+    echo "zoneobjectrename "\"$column1\", \"$column2\""" >> output.txt
 done
 
 
