5640e456d4b894f5ee7b289d29396a6ff5448ba0
Mendu Sai krishna <mskrishna2808@gmail.com>
2023-01-05 15:17:56 +0530

    zone splitting script

--- zone-splitting/zone-splitting.sh
+++ zone-splitting/zone-splitting.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
-echo "============================================================" >> zone-splitting/output.txt
-
 cat zone-splitting/input.txt | while read column1 column2 column3 column4 column5 ; do
-    # SER_NAME=$(echo $column1 | sed 's/_ *..//')
-    # STO_NAME1=$(echo $column3 | sed 's/G *..//')
-    # STO_NAME2=$(echo $column4 | sed 's/G *..//')
+    SER_NAME=$(echo $column1 | sed 's/_ *..//')
+    STO_NAME1=$(echo $column3 | sed 's/G *..//')
+    STO_NAME2=$(echo $column4 | sed 's/G *..//')
     echo "zonecreate "\"$SER_NAME\_$STO_NAME2\", \"$column1\;$column4\""" >> zone-splitting/output.txt
     echo "cfgadd "\"$column5\", \"$SER_NAME\_$STO_NAME2\""" >> zone-splitting/output.txt
     echo "cfg enable "\"$column5\" >> zone-splitting/output.txt
@@ -15,4 +13,3 @@ cat zone-splitting/input.txt | while read column1 column2 column3 column4 column
     echo "zoneobjectrename "\"$column2\", \"$SER_NAME\_$STO_NAME1\""" >> zone-splitting/output.txt
     echo "============================================================" >> zone-splitting/output.txt
 done
-echo "============================================================" >> zone-splitting/output.txt
