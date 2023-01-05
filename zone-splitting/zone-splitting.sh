6f48962fea86c2ba8a8fe174c8ffcadf4bfcbac8
Mendu Sai krishna <mskrishna2808@gmail.com>
2023-01-05 15:52:04 +0530

    zone splitting script

--- zone-splitting/zone-splitting.sh
+++ zone-splitting/zone-splitting.sh
@@ -5,9 +5,9 @@ cat zone-splitting/input.txt | while read column1 column2 column3 column4 column
     echo "zonecreate "\"$SER_NAME\_$STO_NAME2\", \"$column1\;$column4\""" >> zone-splitting/output.txt
     echo "cfgadd "\"$column5\", \"$SER_NAME\_$STO_NAME2\""" >> zone-splitting/output.txt
     echo "cfg enable "\"$column5\" >> zone-splitting/output.txt
-    echo "" >> zone-splitting/output.txt
+    # echo "" >> zone-splitting/output.txt
     echo "zoneremove "\"$column2\", \"$column4\""" >> zone-splitting/output.txt
     echo "cfg enable "\"$column5\" >> zone-splitting/output.txt
     echo "zoneobjectrename "\"$column2\", \"$SER_NAME\_$STO_NAME1\""" >> zone-splitting/output.txt
-    echo "============================================================" >> zone-splitting/output.txt
+    # echo "============================================================" >> zone-splitting/output.txt
 done
