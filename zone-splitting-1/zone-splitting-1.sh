c189757f99cfaa74f08458e3c44271409b8c2046
raghubandreddi <75253716+raghubandreddi@users.noreply.github.com>
2023-01-19 20:48:02 +0530

    Update zone-splitting-1.sh

--- zone-splitting-1/zone-splitting-1.sh
+++ zone-splitting-1/zone-splitting-1.sh
@@ -1,5 +1,5 @@
 cat zone-splitting-1/input.txt | while read column1 column2 column3 column4 column5 ; do
-    SER_NAME=$(echo $column1 | sed 's/_ *..//')
+ #   SER_NAME=$(echo $column1 | sed 's/_ *..//')
     STO_NAME1=$(echo $column3 | sed 's/G *..//')
     STO_NAME2=$(echo $column4 | sed 's/G *..//')
     echo "zonecreate "\"$SER_NAME\_TO_$STO_NAME2\", \"$column1\;$column4\""" >> zone-splitting-1/output.txt
