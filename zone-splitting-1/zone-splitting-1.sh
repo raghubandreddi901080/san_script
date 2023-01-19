6aa0c28f29f6d99acaace5d754af93c4678f9408
Mendu Sai krishna <mskrishna2808@gmail.com>
2023-01-19 21:40:57 +0530

    updated remaining commands

--- zone-splitting-1/zone-splitting-1.sh
+++ zone-splitting-1/zone-splitting-1.sh
@@ -10,7 +10,9 @@ done
 echo "" >> zone-splitting-1/output.txt
 
 cat zone-splitting-1/input.txt | while read column1 column2 column3 column4 column5 ; do
-    SER_NAME=$(echo $column1 | sed 's/_ *..//')
+    SER_NA=$(echo $column1)
+    SER_NAME=${SER_NA%???}
+    # SER_NAME=$(echo $column1 | sed 's/_ *..//')
     STO_NAME1=$(echo $column3 | sed 's/G *..//')
     STO_NAME2=$(echo $column4 | sed 's/G *..//')
     echo "cfgadd "\"$column5\", \"$SER_NAME\_TO_$STO_NAME2\""" >> zone-splitting-1/output.txt
@@ -28,7 +30,9 @@ echo "" >> zone-splitting-1/output.txt
 
 
 cat zone-splitting-1/input.txt | while read column1 column2 column3 column4 column5 ; do
-    SER_NAME=$(echo $column1 | sed 's/_ *..//')
+    SER_NA=$(echo $column1)
+    SER_NAME=${SER_NA%???}
+    # SER_NAME=$(echo $column1 | sed 's/_ *..//')
     STO_NAME1=$(echo $column3 | sed 's/G *..//')
     STO_NAME2=$(echo $column4 | sed 's/G *..//')
     echo "zoneremove "\"$column2\", \"$column4\""" >> zone-splitting-1/output.txt
@@ -45,7 +49,9 @@ echo "" >> zone-splitting-1/output.txt
 echo "" >> zone-splitting-1/output.txt
 
 cat zone-splitting-1/input.txt | while read column1 column2 column3 column4 column5 ; do
-    SER_NAME=$(echo $column1 | sed 's/_ *..//')
+    SER_NA=$(echo $column1)
+    SER_NAME=${SER_NA%???}
+    # SER_NAME=$(echo $column1 | sed 's/_ *..//')
     STO_NAME1=$(echo $column3 | sed 's/G *..//')
     STO_NAME2=$(echo $column4 | sed 's/G *..//')
     echo "zoneobjectrename "\"$column2\", \"$SER_NAME\_TO_$STO_NAME1\""" >> zone-splitting-1/output.txt
