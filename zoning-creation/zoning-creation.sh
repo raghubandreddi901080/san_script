31acfab0deb2a5a9ee33f1ca0d8dbdbe955959f1
Mendu Sai krishna <mskrishna2808@gmail.com>
2023-11-23 10:01:03 +0530

    updated the zone creation script

--- zoning-creation/zoning-creation.sh
+++ zoning-creation/zoning-creation.sh
@@ -1,21 +1,41 @@
-cat zoning-creation/input.txt | while read column1 column2 column3 column4 ; do
-    echo "alicreate "\"$column1\", \"$column2\""" >> zoning-creation/output.txt
-done
+#!/bin/bash
+
+is_empty() {
+    [[ "$1" =~ ^[[:space:]]*$ ]]
+}
+
+while IFS= read -r line; do
+    if is_empty "$line"; then
+        echo "$line" >> zoning-creation/output.txt
+    else
+        read column1 column2 column3 column4 <<< "$line"
+        echo "alicreate "\"$column1\", \"$column2\""" >> zoning-creation/output.txt
+    fi
+done < zoning-creation/input.txt
 
 echo "============================================================" >> zoning-creation/output.txt
 
-cat zoning-creation/input.txt | while read column1 column2 column3 column4 ; do
-    echo "zonecreate "\"$column1\_$column3\", \"$column1\;$column3\""" >> zoning-creation/output.txt
-done
+while IFS= read -r line; do
+    if is_empty "$line"; then
+        echo "$line" >> zoning-creation/output.txt
+    else
+        read column1 column2 column3 column4 <<< "$line"
+        echo "zonecreate "\"$column1\_$column3\", \"$column1\;$column3\""" >> zoning-creation/output.txt
+    fi
+done < zoning-creation/input.txt
 
 echo "============================================================" >> zoning-creation/output.txt
 
-cat zoning-creation/input.txt | while read column1 column2 column3 column4 ; do
-    echo "cfgadd "\"$column4\", \"$column1\_$column3\""" >> zoning-creation/output.txt
-done
+while IFS= read -r line; do
+    if is_empty "$line"; then
+        echo "$line" >> zoning-creation/output.txt
+    else
+        read column1 column2 column3 column4 <<< "$line"
+        echo "cfgadd "\"$column4\", \"$column1\_$column3\""" >> zoning-creation/output.txt
+    fi
+done < zoning-creation/input.txt
 
 echo "============================================================" >> zoning-creation/output.txt
 
 echo "cfg save" >> zoning-creation/output.txt
-
 echo "cfg enable" >> zoning-creation/output.txt
